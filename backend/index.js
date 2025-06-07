adaf571a75583285727d914354ba8de90b2fd5a9
Tkavindi <tharushak77@gmail.com>
2025-06-08 00:03:43 +0530

    add user, course, and enrollment routes to the server

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,11 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const connectDB = require('./utils/connectDB')
+const connectDB = require('./utils/connectDB');
+const userRoute = require('./routes/userRoute');
+const courseRoute = require('./routes/courseRoute');
+const enrollmentRoute = require('./routes/enrollmentRoute');
+
 
 const app = express();
 app.use(express.json());
@@ -13,6 +17,12 @@ app.use(cors());
 
 // Connect to MongoDB
 connectDB();
+
+
+app.use('/api/users', userRoute);
+app.use('/api/courses', courseRoute);
+app.use('/api/enrollments', enrollmentRoute);
+
 
 
 app.listen(port, () =>{
